b87a36943a9eadea0d22722a69493d314f8e099a
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-07 22:18:58 +0530

    [Sharmila] ability to create stack

---
+++ Stack_and_Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿Stack_and_Queue.queue stack = new Stack_and_Queue.queue();
+int option = 1;
+while (option != 2)
+{
+    Console.WriteLine("Enter data to add in Stack: ");
+    int data = Convert.ToInt32(Console.ReadLine());
+    stack.add_Data(data);
+    Console.WriteLine("1.Add Data\n2.Exit");
+    option = Convert.ToInt32(Console.ReadLine());
+}
---
+++ Stack_and_Queue/queue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_Queue
+{
+    internal class queue
+    {
+        Node top;
+        public void Stack_Queue()
+        {
+            this.top = null;
+        }
+        public void add_Data(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("Data added to the top: " + data);
+        }
+    }
+}
