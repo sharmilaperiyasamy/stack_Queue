a09464387335b52a769eacb03fcdc67f57f10a99
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-07 22:27:58 +0530

    [Sharmila] peak pop from the stack

--- Stack_and_Queue/Program.cs
+++ Stack_and_Queue/Program.cs
@@ -1,10 +1,26 @@
 ﻿Stack_and_Queue.queue stack = new Stack_and_Queue.queue();
-int option = 1;
-while (option != 2)
+Console.WriteLine("Stack Operations\n0.Exit\n1.Push the Data into the stack\n2.Pop Data from the stack\n3.Display Peak\nEnter your choice:");
+int option = Convert.ToInt32(Console.ReadLine());
+while (option != 0)
 {
-    Console.WriteLine("Enter data to add in Stack: ");
-    int data = Convert.ToInt32(Console.ReadLine());
-    stack.add_Data(data);
-    Console.WriteLine("1.Add Data\n2.Exit");
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine("Enter data to add in Stack: ");
+            int data = Convert.ToInt32(Console.ReadLine());
+            stack.add_Data(data);
+            break;
+        case 2:
+            stack.pop_data();
+            break;
+        case 3:
+            stack.peak_data();
+            break;
+        default:
+            Console.WriteLine("Invalid Chioce.");
+            break;
+    }
+    Console.WriteLine("Stack Operations\n0.Exit\n1.Push the Data into the stack\n2.Pop Data from the stack\n3.Display Peak\nEnter your choice:");
     option = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\n");
 }
--- Stack_and_Queue/queue.cs
+++ Stack_and_Queue/queue.cs
@@ -27,5 +27,23 @@ namespace Stack_and_Queue
             this.top = node;
             Console.WriteLine("Data added to the top: " + data);
         }
+        public void peak_data()
+        {
+            if (this.top == null)
+                Console.WriteLine("Stack is Empty.");
+            else
+                Console.WriteLine("Data at the peak of stack is: " + this.top.data + "\n");
+        }
+        public void pop_data()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty.");
+                return;
+            }
+            Console.WriteLine(this.top.data + " is popped from stack.\n");
+            this.top = this.top.next;
+            peak_data();
+        }
     }
 }
